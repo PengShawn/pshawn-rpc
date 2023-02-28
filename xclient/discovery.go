6889c8749ebf2283bc6e816c9cfc63325ab36c99
pshawn <pshawn@126.com>
2023-03-01 01:46:47 +0800

    feat:服务发现模块

---
+++ xclient/discovery.go
@@ -0,0 +1,86 @@
+package xclient
+
+import (
+	"errors"
+	"math"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+type SelectMode int
+
+const (
+	RandomSelect     SelectMode = iota // select randomly
+	RoundRobinSelect                   // select using round-robin algorithm
+)
+
+// Discovery is the interface that wraps the basic discovery methods.
+type Discovery interface {
+	Refresh() error
+	Update(servers []string) error
+	Get(mode SelectMode) (string, error)
+	GetAll() ([]string, error)
+}
+
+// MultiServerDiscovery is a discovery for multiple servers without a register center.
+// user provides the address list explicitly instead.
+type MultiServerDiscovery struct {
+	r       *rand.Rand   // generate random number
+	mu      sync.RWMutex // protect following
+	servers []string     // server list
+	index   int          // record the selected index for round-robin algorithm
+}
+
+func NewMultiServerDiscovery(servers []string) *MultiServerDiscovery {
+	d := &MultiServerDiscovery{
+		servers: servers,
+		r:       rand.New(rand.NewSource(time.Now().UnixNano())),
+	}
+	d.index = d.r.Intn(math.MaxInt32 - 1)
+	return d
+}
+
+var _ Discovery = (*MultiServerDiscovery)(nil)
+
+// Refresh doesn't make sense for MultiServerDiscovery, so ignore it
+func (d *MultiServerDiscovery) Refresh() error {
+	return nil
+}
+
+// Update the servers of discovery dynamically if needed
+func (d *MultiServerDiscovery) Update(servers []string) error {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	d.servers = servers
+	return nil
+}
+
+// Get a server according to mode
+func (d *MultiServerDiscovery) Get(mode SelectMode) (string, error) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	n := len(d.servers)
+	if n == 0 {
+		return "", errors.New("rpc discovery: no available servers")
+	}
+	switch mode {
+	case RandomSelect:
+		return d.servers[d.r.Intn(n)], nil
+	case RoundRobinSelect:
+		server := d.servers[d.index%n]
+		d.index = (d.index + 1) % n
+		return server, nil
+	default:
+		return "", errors.New("rpc discovery: not supported select mode")
+	}
+}
+
+// GetAll returns all servers
+func (d *MultiServerDiscovery) GetAll() ([]string, error) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	servers := make([]string, len(d.servers), len(d.servers))
+	copy(servers, d.servers)
+	return servers, nil
+}
